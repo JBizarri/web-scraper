a5d5db4f015f1853a7d5cf5f5f87ef90f0340cfc
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2024-03-21 01:28:30 -0300

    feat: add context manager for connection

--- src/database/database.py
+++ src/database/database.py
@@ -1,12 +1,24 @@
 import sqlite3
+from contextlib import contextmanager
 from typing import Optional
 
 
 class Database:
     def __init__(self, path: Optional[str] = None) -> None:
-        self._connection = sqlite3.connect(path or ":memory:", check_same_thread=False)
-        self._connection.row_factory = sqlite3.Row
+        self._path = path
 
     @property
+    @contextmanager
     def connection(self):
-        return self._connection
+        connection = sqlite3.connect(self._path or ":memory:", check_same_thread=False)
+        connection.row_factory = sqlite3.Row
+
+        try:
+            yield connection
+        except Exception:
+            connection.rollback()
+            connection.close()
+        else:
+            connection.commit()
+        finally:
+            connection.close()
